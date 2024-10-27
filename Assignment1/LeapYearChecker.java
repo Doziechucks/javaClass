d4c0e4c8891e8332c0f63e4ca8fc54ab8d928678
Doziechucks <chiedoziechukwukam@gmail.com>
2024-10-28 00:35:39 +0100

    consonant vowel checker

--- Assignment1/LeapYearChecker.java
+++ Assignment1/LeapYearChecker.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
-public class Multiplier{
+public class LeapYearChecker{
 public static void main(String[] args){
    Scanner input = new Scanner(System.in);
    System.out.print("enter number: " );
    int yearInput = input.nextInt();
    if (yearInput % 4 == 0 ){
-      System.out.printf("%d%n is  a leap year", yearInput);}
+      System.out.printf("%d is a leap year %n", yearInput);}
    else {
       System.out.printf("%d is not a leap year %n", yearInput);}
 }
