63901d0770ce187902472d2a4fc6ab5d98530cc4
Doziechucks <chiedoziechukwukam@gmail.com>
2024-10-28 02:34:37 +0100

    multiplier number input with integers till they get to inputed number

--- Assignment1/Multiplier.java
+++ Assignment1/Multiplier.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 public class Multiplier{
 public static void main(String[] args){
    Scanner input = new Scanner(System.in);
-   System.out.print("enter number :" );
-   for  
+   System.out.print("enter number: " );
+   int numberInput = input.nextInt();
+
+   for(int i = 0; i <= numberInput; i ++ ){
+      int product = numberInput * i ; 
+      System.out.printf(" %d x %d = %d%n",numberInput, i, product);
+      
+}
+      
 }} 
