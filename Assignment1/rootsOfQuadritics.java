777f1ad6c919c1dbb7215523dd829b64b4f08d98
Doziechucks <chiedoziechukwukam@gmail.com>
2024-10-28 20:06:29 +0100

    pseudocodes

--- Assignment1/rootsOfQuadritics.java
+++ Assignment1/rootsOfQuadritics.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Multiplier{
+public class RootsOfQuadratics{
 public static void main(String[] args){
    Scanner input = new Scanner(System.in);
    System.out.print("Enter a: " );
