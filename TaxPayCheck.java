71093150221b463e5c10c24098f5a13186309e19
Doziechucks <chiedoziechukwukam@gmail.com>
2024-10-28 14:06:12 +0100

    tax pay

--- TaxPayCheck.java
+++ TaxPayCheck.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
-public class TaxChecker{
+public class TaxPayCheck{
 public static void main(String[] args){
    Scanner input = new Scanner(System.in);
    System.out.print("Enter Wage: " );
    int numberInput = input.nextInt();
-double tax;
-   for (1 = 0if (numberInput <= 15000){
-      tax = numberInput * 0.15 ;}
+   double tax;
+   if (numberInput <= 15000){
+   tax = numberInput * 0.15 ;}
    else {
-      tax = numberInput * 0.15 ;}
-System.out.printf("your tax is: %f",tax);
+   tax = numberInput * 0.15 ;}
+System.out.printf("your tax is: %f%n",tax);
 }
 }
