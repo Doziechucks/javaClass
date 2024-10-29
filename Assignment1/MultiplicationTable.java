4eb9416671d130e70b8ac988f0321b9a298b984d
Doziechucks <chiedoziechukwukam@gmail.com>
2024-10-29 08:02:58 +0100

    multiplication table

---
+++ Assignment1/MultiplicationTable.java
@@ -0,0 +1,51 @@
+public class Multiplier{
+public static void main(String[] args){
+
+   for(int i = 1; i <= 12; i ++ ){
+      int product = 1 * i ; 
+      System.out.printf(" 1 x %d = %d%n", i, product);}
+      System.out.println();
+
+   for(int i = 1; i <= 12; i ++ ){
+      int product = 2 * i ; 
+      System.out.printf(" 2 x %d = %d%n", i, product);}
+      System.out.println();
+
+   for(int i = 1; i <= 12; i ++ ){
+      int product = 3 * i ; 
+      System.out.printf(" 3 x %d = %d%n", i, product);}
+      System.out.println();
+
+   for(int i = 1; i <= 12; i ++ ){
+      int product = 4 * i ; 
+      System.out.printf(" 4 x %d = %d%n", i, product);}
+      System.out.println();
+
+   for(int i = 1; i <= 12; i ++ ){
+      int product = 5 * i ; 
+      System.out.printf(" 5 x %d = %d%n", i, product);}
+
+   for(int i = 1; i <= 12; i ++ ){
+      int product = 6 * i ; 
+      System.out.printf(" 6 x %d = %d%n", i, product);}
+      System.out.println();
+
+   for(int i = 1; i <= 12; i ++ ){
+      int product = 7 * i ; 
+      System.out.printf(" 7 x %d = %d%n", i, product);}
+      System.out.println();
+
+   for(int i = 1; i <= 12; i ++ ){
+      int product = 8 * i ; 
+      System.out.printf(" 8 x %d = %d%n", i, product);}
+      System.out.println();
+
+   for(int i = 1; i <= 12; i ++ ){
+      int product = 9 * i ; 
+      System.out.printf(" 9 x %d = %d%n", i, product);}
+      System.out.println();
+
+
+
+}
+}   
