63d1dcb8c867dd564610e961d0e03425cf60599e
Doziechucks <chiedoziechukwukam@gmail.com>
2024-12-06 09:02:02 +0100

    Update to Banke Atm

--- gateOne/BankeAtm.java
+++ gateOne/BankeAtm.java
@@ -9,7 +9,7 @@ public class BankeAtm{
    
       ArrayList<String> names = new ArrayList<String>();
       ArrayList<String> accountNumbers = new ArrayList<String>();
-      ArrayList<String> passWord = new ArrayList<String>();
+      ArrayList<String> passWords = new ArrayList<String>();
       ArrayList<Double> balances = new ArrayList<Double>();
 
       int exitGuy = -1;
@@ -19,34 +19,36 @@ public class BankeAtm{
          exitGuy = input.nextInt();
 
          if(exitGuy == 1){
-            System.out.print("Enter first name: ");
-            String firstName = input.next();
+            createAccount();
+                 }               
+         returnMainMenu();
+         exitGuy = input.nextInt();
 
-            System.out.print("Enter last name: ");
-            String lastName = input.next();
-
-            String fullName = "" + firstName + " " + lastName + "";
-            names.add(fullName); 
-        
-            String accountNumber = "";
-            for(int number = 0; number < 10; number++){
-               int Number = random.nextInt(9);
-               accountNumber = ""+ Number +"";
-		}
-            accountNumbers.add(accountNumber); 
-
-            System.out.print("Choose a 4 digit password");
-            String pass = input.next();
-            while (pass.length() != 4){
-               System.out.print("Choose a 4 digit password");
-               pass = input.next();
+ 
+           if(exitGuy == 1){
+              createAccount();
          	}
-            passWord.add(pass);
-            double balance = 0;
-            balances.add(balance);
-                                
-        System.out.print("what would like to do?"); 
-        System.out.print("""
+              returnMainMenu();
+              exitGuy = input.nextInt();
+      
+           
+
+           if(exitGuy == 2){ 
+              System.out.print("Login your"})
+              System.out.print("how much do you want to deposit: ");
+              double moneyInput = input.nextDouble();
+              balances.add(0, moneyInput);
+              System.out.printf("your balance is now: %s%n", balances.get(0));
+              returnMainMenu();
+                  
+		}
+           
+         }
+	
+	}
+      public static void returnMainMenu(){
+         System.out.print("what would you like to do?"); 
+         System.out.print("""
 1. Create an account with their first name and last name and pin
 2. Close account.
 3. Deposit money
@@ -55,49 +57,51 @@ public class BankeAtm{
 6. Transfer from one account to another.
 7. Change Pin.
 0. to quit
-""");
-         exitGuy = input.nextInt();
+""");}
+      public static void createAccount(){
+         System.out.print("Enter first name: ");
+         String firstName = input.next();
 
-        switch(exitGuy){
-           case 1:
-              System.out.print("Enter first name: ");
-              firstName = input.next();
+         System.out.print("Enter last name: ");
+         String lastName = input.next();
 
-              System.out.print("Enter last name: ");
-              lastName = input.next();
-
-              fullName = "" + firstName + " " + lastName + "";
-              names.add(fullName); 
-
-              accountNumber = "";
-              for(int number = 0; number < 10; number++){
-                 int Number = random.nextInt(9);
-                 accountNumber = ""+ Number +"";
-                   }
-              accountNumbers.add(accountNumber);
-
-              System.out.print("Choose a 4 digit password");
-              pass = input.next();
-              while (pass.length() != 4){
-                 System.out.print("Choose a 4 digit password");
-                 pass = input.next();
-                	}
-              passWord.add(pass);
-              balance = 0;
-              balances.add(balance);
-           break;  
-
-           case 2:
-              System.out.print("how much do you want to deposit: ");
-              double moneyInput = input.nextDouble();
-              balances.add(0, moneyInput);
-
-                  }
-		}
-           
-         }
-	
-	}
+         String fullName = "" + firstName + " " + lastName + "";
+         names.add(fullName); 
         
+         String accountNumber = "";
+         for(int number = 0; number < 10; number++){
+            int Number = random.nextInt(9);
+            accountNumber = ""+ Number +"";
+		}
+         accountNumbers.add(accountNumber); 
+
+         System.out.print("Choose a 4 digit password");
+         String pass = input.next();
+         while (pass.length() != 4){
+            System.out.print("Choose a 4 digit password");
+            pass = input.next();
+          	}
+         passWord.add(pass);
+         double balance = 0;
+         balances.add(balance);
+		}
+      
+
+      public static void logIn(){
+   System.out.print("Enter you Account Number: ");
+   int accountNumberCheck = input.nextInt()
+   int checker = -1
+   for(int number =0; number < accountNumbers.size(); number++){
+      if (balances.get(number) == accountNumberCheck){
+         checker = number;
+		}
+	}  
+   if (checker == -1){
+      System.out.print("Invalid account number");
+	}
+
+}     
+
+
 }
 
