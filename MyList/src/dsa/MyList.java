6a84e1318d7e1a1e1cd3c021a628abce680c48fe
Doziechucks <chiedoziechukwukam@gmail.com>
2025-01-22 20:53:44 +0100

    My list to an extent

---
+++ MyList/src/dsa/MyList.java
@@ -0,0 +1,66 @@
+package dsa;
+import java.lang.StringBuilder;
+public class MyList {
+    private int size;
+    private int capacity;
+    private String[] words = new String[3];
+
+    public String[] add(String word) {
+        words[size++] = word;
+        if (size == capacity) increaseCapacity(size);
+
+        return words;
+    }
+
+    public String[] increaseCapacity(int size) {
+        String[] elements = new String[size * 2];
+        for (int number = 0; number < size; number++) {
+            elements[number] = words[number];
+        }
+        words = elements;
+        return words;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        sb.append("\"");
+        for (int number = 0; number < size; number++) {
+            if (number < size) {
+                sb.append(words[number]);
+                sb.append("\", \"");
+            } else sb.append(words[number]);
+        }
+        sb.append("]");
+
+        return sb.toString();
+    }
+
+    public boolean contains(String word) {
+        for (int number = 0; number < size; number++) {
+            if (word.equals(words[number])) return true;
+            else return false;
+        }
+        return false;
+    }
+
+
+  public String[] remove(String item) {
+        if (words.contains() == true) words[index()]) = null;
+            for(int number = index; number < size){
+                words[number] = words[number + 1]
+            }
+
+        return words;
+        }
+
+}
+
---
+++ MyList/test/dsa/MyListTest.java
@@ -0,0 +1,55 @@
+package dsa;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MyListTest {
+    private MyList myList;
+
+    @BeforeEach
+    public void setMyList() {
+        myList = new MyList();
+    }
+    @Test
+    public void test_ifListEmpty(){
+        assertTrue(0== myList.size());
+
+    }
+
+    @Test
+    public void test_sizeWhenElementIsAdded(){
+        myList.add("G-String");
+        assertTrue(1 == myList.size());
+    }
+
+    @Test
+    public void test_toSeeElementAdded() {
+        myList.add("A-String");
+        myList.add("B-String");
+        String actual = myList.toString();
+        String expected = "[\"A-String\", \"B-String\"]";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test_ifcontainswork(){
+        myList.add("mango");
+        myList.add("Orange");
+        assertTrue(myList.contains("mango"));
+    }
+
+   @Test
+    public void test_toRemoveElement(){
+        myList.add("mango");
+        myList.add("Orange");
+        myList.add("Apple");
+        myList.remove("Orange");
+        assertTrue(myList.toString() == "[\"mango\", \"Apple\"]");
+    }
+
+
+}
