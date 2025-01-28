3acde4ea084f5478c1039cd778b622e6b8277451
Doziechucks <chiedoziechukwukam@gmail.com>
2025-01-28 01:40:19 +0100

    git commit Account class

---
+++ BankAppChibuzor/src/bankApp/Account.java
@@ -0,0 +1,8 @@
+package bankApp;
+
+public class Account {
+    private String password;
+    private Double balance;
+
+
+}
---
+++ BankAppChibuzor/test/bankApp/AccountTest.java
@@ -0,0 +1,14 @@
+package bankApp;
+
+i
+import org.junit.jupiter.api.BeforeEach;
+
+public class AccountTest {
+    Account account;
+
+    @BeforeEach
+    public void setAccount(){
+        account = new Account();
+    }
+
+}
