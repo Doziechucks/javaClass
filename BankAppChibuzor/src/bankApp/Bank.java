c961aebc4a7e2ba48d074a579d3e9666856c1878
Doziechucks <chiedoziechukwukam@gmail.com>
2025-01-30 19:33:19 +0100

    git commit Account class

--- BankAppChibuzor/src/bankApp/Bank.java
+++ BankAppChibuzor/src/bankApp/Bank.java
@@ -2,11 +2,35 @@ package bankApp;
 
 import java.util.ArrayList;
 
-public class Bank {
-    public ArrayList<int> accountNumbers = new ArrayList<int>();
+public class Bank{
+    public ArrayList<String> accountNumbers = new ArrayList<String>();
     public ArrayList<String> firstNames = new ArrayList<String>();
     public ArrayList<String> lastNames = new ArrayList<String>();
     private ArrayList<String> passwords= new ArrayList<String>();
+    public ArrayList<Double> balances = new ArrayList<Double>();
+    private int index;
 
-    public void
+    public String getPassword(int index){
+        return passwords.get(index);
+    }
+
+    public void addAccount(String accountnumber){
+        accountNumbers.add(accountnumber);
+
+    }
+
+    public String getAccountNumber(int index){
+        return accountNumbers.get(index);
+        }
+
+    public int getIndex(String accountNumber){
+
+        for(int number = 0; number < accountNumbers.size(); number++){
+            if(accountNumber.equals(accountNumbers.get(number))) index = number;
+            return index;
+
+        }
+        throw new IllegalArgumentException("Incorrect details");
+    }
+
 }
--- BankAppChibuzor/test/bankApp/BankTest.java
+++ BankAppChibuzor/test/bankApp/BankTest.java
@@ -1,7 +1,25 @@
 package bankApp;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 
-public class BankTest {
+import static org.junit.Assert.assertEquals;
 
+public class BankTest {
+    Bank bank;
+    @BeforeEach
+    public void setBank(){
+        bank = new Bank();
+    }
+
+    @Test
+    public void test_ifMyAccountNumberIsAdded(){
+        bank.addAccount("2323232323");
+        String actual = bank.getAccountNumber(bank.getIndex());
+        String expected = "2323232323";
+        assertEquals(actual, expected);
+
+    }
 }
